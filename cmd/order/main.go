1c8bf6ab1d4d391b90184780a020a73d304ba441
PosokhovVadim <posokhovvadyusha@gmail.com>
2024-06-07 14:11:06 +0400

    init base config

--- cmd/order/main.go
+++ cmd/order/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"order/internal/config"
+	"order/pkg/logger"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func run() error {
+	err := godotenv.Load("local.env")
+	if err != nil {
+		fmt.Printf("Error loading environment: %v\n", err)
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+
+	cfg, err := config.New(configPath)
+
+	if err != nil {
+		return err
+	}
+
+	_ = cfg
+
+	log, err := logger.SetupLogger(cfg)
+	if err != nil {
+		return err
+	}
+
+	_ = log
+
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := run(); err != nil {
+		fmt.Printf("Error running service: %v\n", err)
+		os.Exit(1)
+	}
 }
